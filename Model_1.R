b7f073553674addd20229053da099bea7e85b9e0
Andrew Maloney <amaloney@iastate.edu>
2020-11-18 22:08:39 -0600

    Cleaned Up Code

--- Model_1.R
+++ Model_1.R
@@ -1,5 +1,5 @@
 
-set.seed(777)
+set.seed(777777) # Set Large Seed.  Seed must be set before loading keras & Tensorflow
 
 library(keras)
 library(tensorflow)
@@ -8,7 +8,7 @@ library(abind)
 
 
 
-cnn_model_2 <- keras_model_sequential() %>%
+cnn_model <- keras_model_sequential() %>%
   layer_conv_3d(filters = 32, kernel_size = c(3, 3, 1), input_shape = c(128, 128, 1, 1)) %>%
   layer_zero_padding_3d() %>%
   layer_activation_relu() %>%
@@ -31,55 +31,28 @@ cnn_model_2 <- keras_model_sequential() %>%
 
 
 callbacks_list <- list(
-  callback_model_checkpoint(filepath = "my_model.h5", monitor = "val_loss",save_best_only = TRUE),
-  callback_reduce_lr_on_plateau(monitor = "val_loss", factor = 0.1, patience = 10))
+  callback_model_checkpoint(filepath = "my_model.h5", monitor = "val_loss",save_best_only = TRUE),# Save the best Model out of all epochs
+  callback_reduce_lr_on_plateau(monitor = "val_loss", factor = 0.1, patience = 10)) # Adjust Learning rate if there is no improvement
 
 
 
-cnn_model_2 %>% compile(optimizer = "rmsprop",
+cnn_model %>% compile(optimizer = "rmsprop",
                       loss = "categorical_crossentropy",
                       metrics = c("accuracy"))
 
 
-x_test_2 <- array_1_1[1:2000, , ,1, 1]
-x_test <- array(c(x_test),
-                dim = c(2000, 128, 128, 1, 1))
 
-y_test <- y_1_1[1:2000,]
+array_1_x <- abind(array_1_0, array_1_1, array_1_2, array_1_3, along=1) # Full Training Data.  
+
+y_1_x <- rbind(y_1_0, y_1_1, y_1_2, y_1_3) # Full Training Data
 
 
-cnn_model_2 %>% fit(array_1_0, y_1_0, 
+
+
+cnn_model %>% fit(array_1_x, y_1_x, 
                     batch_size = 100,
                     epochs = 5,
-                    validation_data = list(x_test, y_test),
+                    validation_data = list(array_1_4, y_1_4), # Data used for Validation
                     callbacks = callbacks_list)
 
 
-
-df <- as.data.frame(rbind(y_1_0, y_1_1, y_1_2, y_1_3, y_1_4))
-
-head(df)
-
-#V1 <- Good striation
-#V2 <- Right & Left Groove
-#V3 <- Vertical Striations
-#V4 <- No Striation
-#V5 <- Damage
-#V6 <- Breakoff
-
-df <- df %>%
-  rename(Well_Expressed_Striae = V1,
-         Grooves = V2,
-         Vertical_Striae = V3,
-         No_Striae = V4,
-         Damage = V5,
-         Breakoff = V6)
-
-
-df <- gather(df, Annotation, Value, Well_Expressed_Striae:Breakoff)
-
-df %>% group_by(Annotation) %>%
-  summarise(sum = sum(Value)) %>%
-    ggplot(aes(x = Annotation, y = sum))+
-      geom_bar(stat = "identity", fill = "orange")+
-        theme_bw()+ xlab("Marking Scan Annotations")+ylab("# of Occurences")+ggtitle("Class Imbalance")
